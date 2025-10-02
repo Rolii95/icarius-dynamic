49b7d87bf6a2de179dbbc6104de1f74c58350f2b
Rolii95 <108462167+Rolii95@users.noreply.github.com>
2025-10-02 01:19:48 +0100

    Update import path for globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '@/styles/globals.css'
 import { ConsentProvider } from '@/components/consent-provider'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
